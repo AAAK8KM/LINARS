d7c068861f5cb895e384660338f2ee333cf9a696
Daniil10001 <daniiltimohin8@gmail.com>
2026-02-22 18:14:46 +0300

    type update

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -21,21 +21,21 @@ template<typename dtype>
 class Imatrix
 {
     public:
-        virtual dtype& ge(const std::size_t i, const std::size_t j) = 0;
-        virtual const dtype& ge(const std::size_t i, const std::size_t j) const = 0;
+        virtual dtype& ge(const std::uint32_t i, const std::uint32_t j) = 0;
+        virtual const dtype& ge(const std::uint32_t i, const std::uint32_t j) const = 0;
         
-        virtual dtype gev(const std::size_t i, const std::size_t j) const = 0;
+        virtual dtype gev(const std::uint32_t i, const std::uint32_t j) const = 0;
 
-        inline virtual const std::pair<std::size_t, std::size_t> size() const = 0;
+        inline virtual const std::pair<std::uint32_t, std::uint32_t> size() const = 0;
         //virtual dtype det();//later
 
-        Matrix<dtype> operator*(const Imatrix& B) const
+        virtual Matrix<dtype> operator*(const Imatrix& B) const
         {
             if (this->size().second!=B.size().first) throw std::runtime_error("Matrixes has worng sizes. Can not multiply!");
             Matrix<dtype> C(this->size().first,B.size().second);
-            for (std::size_t i=0;i<this->size().first;i++)
-                for (std::size_t j=0;j<B.size().second;j++)
-                    for (std::size_t k=0;k<this->size().first;k++)
+            for (std::uint32_t i=0;i<this->size().first;i++)
+                for (std::uint32_t j=0;j<B.size().second;j++)
+                    for (std::uint32_t k=0;k<this->size().first;k++)
                         C.ge(i, j)+=this->gev(i, k)*B.gev(k, j);
             return C;
         }
@@ -45,8 +45,8 @@ class Imatrix
             if (this->size().second!=B.size().second) return false;
             if (this->size().first!=B.size().first) return false;
 
-            for (std::size_t i=0;i<this->size().first;i++)
-                for (std::size_t j=0;j<this->size().second;j++)
+            for (std::uint32_t i=0;i<this->size().first;i++)
+                for (std::uint32_t j=0;j<this->size().second;j++)
                     if (std::abs(this->gev(i,j)-B.gev(i, j))>1e-9)
                         return false;
             return true;
@@ -73,21 +73,19 @@ class Matrix: public Imatrix<dtype>
         std::uint32_t n,m;
     public:
         Matrix(){}
-        Matrix(std::uint32_t n, std::uint32_t m):data(n*m,0),n(n),m(m){}
+        Matrix(std::uint32_t n_, std::uint32_t m_):data(n_*m_,0),n(n_),m(m_){}
         Matrix(std::pair<std::uint32_t, std::uint32_t> s):Matrix(s.first,s.second){}
         Matrix(Imatrix<dtype>& M){
             auto p=M.size();
-            
-            if (std::max(p.first,p.second)>UINT32_MAX)  throw std::runtime_error("Too big matrix!");
-            
-            this->n=p.first;
-            this->m=p.second;
+
+            this->n=static_cast<std::uint32_t>(p.first);
+            this->m=static_cast<std::uint32_t>(p.second);
 
             this->data.resize(m*n);
 
-            for (std::size_t i=0;i<p.first;i++)
-                for (std::size_t j=0;j<p.second;j++)
-                    this->data[i*p.second+j]=M.gev(i, j);
+            for (std::uint32_t i=0;i<p.first;i++)
+                for (std::uint32_t j=0;j<p.second;j++)
+                    this->data[static_cast<std::size_t>(i)*p.second+j]=M.gev(i, j);
                 
         }
 
@@ -113,22 +111,22 @@ class Matrix: public Imatrix<dtype>
             return *this;
         }
 
-        dtype& ge(const std::size_t i, const std::size_t j)
+        dtype& ge(const std::uint32_t i, const std::uint32_t j)
         {
-            return data[i*m+j];
+            return data[static_cast<std::size_t>(i)*m+j];
         };
 
-        const dtype& ge(const std::size_t i, const std::size_t j) const
+        const dtype& ge(const std::uint32_t i, const std::uint32_t j) const
                 {
-            return data[i*m+j];
+            return data[static_cast<std::size_t>(i)*m+j];
         };
         
-        dtype gev(const std::size_t i, const std::size_t j) const
+        dtype gev(const std::uint32_t i, const std::uint32_t j) const
         {
-            return data[i*m+j];
+            return data[static_cast<std::size_t>(i)*m+j];
         };
 
-        inline const std::pair<std::size_t, std::size_t> size() const {return std::make_pair(n, m);};
+        inline const std::pair<std::uint32_t, std::uint32_t> size() const {return std::make_pair(n, m);};
 
         ~Matrix(){};
 };
@@ -141,12 +139,12 @@ class M3diag: public Imatrix<dtype>
         std::vector<dtype> data;
     public:
         M3diag(){}
-        M3diag(std::size_t n):data(3*n,0){}
+        M3diag(std::uint32_t n):data(3*n,0){}
         M3diag(Imatrix<dtype>& m):data(3*m.size().first,0){
             auto p=m.size();
             if (p.first!=p.second) throw std::runtime_error("Can not create 3 diag matrix from not square!");
-            for (std::size_t i=0;i<p.first;i++)
-                for (std::size_t j=0;j<p.first;j++)
+            for (std::uint32_t i=0;i<p.first;i++)
+                for (std::uint32_t j=0;j<p.first;j++)
                 {
                     if ((i > j ? i - j : j - i) > 1)
                     {
@@ -157,59 +155,45 @@ class M3diag: public Imatrix<dtype>
                     else
                     {
                         if (i-j==1)
-                            this->data[i*3-1]=m.gev(i, j);
+                            this->data[static_cast<std::size_t>(i)*3-1]=m.gev(i, j);
                         if (i==j)
-                            this->data[i*3]=m.gev(i, j);
+                            this->data[static_cast<std::size_t>(i)*3]=m.gev(i, j);
                         if (j-i==1)
-                            this->data[i*3+1]=m.gev(i, j);
+                            this->data[static_cast<std::size_t>(i)*3+1]=m.gev(i, j);
                     }
                 }
         }
         M3diag(M3diag& m):data(m.data){}
-        M3diag& operator=(const M3diag& m)
-        {
-            this->data=m.data;
-            return *this;
-        }
-        M3diag(M3diag&& m)
-        {
-            this->data=std::move(m.data);
-        };
-        M3diag& operator=(const M3diag&& m)
-        {
-            this->data=std::move(m.data);
-            return *this;
-        }
 
-        dtype& ge(const std::size_t i, const std::size_t j)
+        dtype& ge(const std::uint32_t i, const std::uint32_t j)
         {
             if ((i > j ? i - j : j - i) > 1)
             {
                 std::cout<<i<<" "<<j<<std::endl;
                 throw std::runtime_error("Not valid element!");
             }
-            return data[2*i+j];
+            return data[2*static_cast<std::size_t>(i)+j];
             //else return data[3*i+1];
         };
 
-        const dtype& ge(const std::size_t i, const std::size_t j) const
+        const dtype& ge(const std::uint32_t i, const std::uint32_t j) const
                 {
             if ((i > j ? i - j : j - i) > 1)
             {
                 std::cout<<i<<" "<<j<<std::endl;
                 throw std::runtime_error("Not valid element!.");
             }
-            return data[2*i+j];
+            return data[2*static_cast<std::size_t>(i)+j];
         };
         
-        dtype gev(const std::size_t i, const std::size_t j) const
+        dtype gev(const std::uint32_t i, const std::uint32_t j) const
         {
             if ((i > j ? i - j : j - i) > 1) return 0;
             //if (i>j) return data[4*i-j];
-            return data[2*i+j];
+            return data[2*static_cast<std::size_t>(i)+j];
         };
 
-        inline const std::pair<std::size_t, std::size_t> size() const {return std::make_pair(data.size()/3, data.size()/3);};
+        inline const std::pair<std::uint32_t, std::uint32_t> size() const {return std::make_pair(data.size()/3, data.size()/3);};
 
         void flush()
         {
